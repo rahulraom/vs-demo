81ebdbd70cf13fa8d446ce2818076e647d679271
rahulraom <rahulraom.proceedit@gmail.com>
2025-04-24 22:26:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,37 @@
 const { createAgent } = require('@veramo/core')
-const { KeyManager } = require('@veramo/key-manager')
 const { DIDManager } = require('@veramo/did-manager')
+const { KeyManager } = require('@veramo/key-manager')
+const { MemoryPrivateKeyStore } = require('@veramo/kms-local')
+const { KeyManagementSystem } = require('@veramo/kms-local')
 const { CredentialIssuer } = require('@veramo/credential-w3c')
+const { DIDResolverPlugin } = require('@veramo/did-resolver')
+const { getDidResolver } = require('did-resolver')
+const { EthrDIDProvider } = require('@veramo/did-provider-ethr')
 
 const agent = createAgent({
   plugins: [
-    new KeyManager({}),
-    new DIDManager({}),
+    new KeyManager({
+      store: new MemoryPrivateKeyStore(),
+      kms: {
+        local: new KeyManagementSystem(),
+      },
+    }),
+    new DIDManager({
+      providers: {
+        'did:ethr': new EthrDIDProvider({
+          defaultKms: 'local',
+          network: 'goerli', // use testnet
+          rpcUrl: 'https://rpc.ankr.com/eth_goerli' // or any Goerli RPC
+        }),
+      },
+      defaultProvider: 'did:ethr',
+    }),
     new CredentialIssuer(),
-  ]
+    new DIDResolverPlugin({
+      resolver: getDidResolver()
+    })
+  ],
 })
 
-async function issueCredential() {
-  const did = 'did:web:rahul.proceedit.com'
-  const credential = await agent.createVerifiableCredential({
-    credential: {
-      issuer: { id: did },
-      credentialSubject: {
-        id: 'did:web:holder.user.com',
-        role: 'Intern',
-        verified: true,
-      },
-    },
-    proofFormat: 'jwt',
-  })
-
-  console.log(JSON.stringify(credential, null, 2))
-}
-
-issueCredential()
+// EXPORT AGENT FOR USE IN OTHER FILES
+module.exports = { agent }
